3ae2eac3218ac9a50ef79b7ac25b2cfabc991513
吴旻轩 <846127528@qq.com>
2021-03-06 14:55:29 +0800

    springboot框架

---
+++ src/test/java/com/codemonkeys/backendcoin/BackendCoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemonkeys.backendcoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCoinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
