d4a69a2550bbc5c664d2ac17f57d0d9fc0633352
shichenhang <181250124@smail.nju.edu.cn>
2021-03-11 14:34:54 +0800

    chore: springboot关闭和启动脚本

---
+++ stop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Stopping SpringBoot Application"
+# shellcheck disable=SC2006
+pid=`ps -ef | grep '.*backend-coin.*jar' | grep -v grep | awk '{print $2}'`
+if [ -n "$pid" ]
+then
+   kill -9 "$pid"
+   rm -f nohup.out
+fi
