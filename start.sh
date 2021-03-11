b29cbb9d97dfb732c4d6398648e55e4efe936d7e
shichenhang <181250124@smail.nju.edu.cn>
2021-03-11 15:48:34 +0800

    chore: 测试jenkins hook

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "starting springboot application"
+echo "Starting SpringBoot Application"
 pwd
 export JENKINS_NODE_COOKIE=dontKillMe
 nohup java -Dhudson.util.ProcessTree.disable=true -jar target/*.jar &
