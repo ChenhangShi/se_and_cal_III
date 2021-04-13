b5e15d0cebbc783b1e99daf425fb4a4349bae25e
shichenhang <181250124@smail.nju.edu.cn>
2021-04-13 23:37:20 +0800

    chore: 解决部署不运行中

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo "Starting SpringBoot Application"
-pwd
 nohup java -jar /var/lib/jenkins/workspace/backend-coin/target/backend-coin-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod &
