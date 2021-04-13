3f726430af10d6d81390a1465f3db72c9c894f10
shichenhang <181250124@smail.nju.edu.cn>
2021-04-13 22:55:13 +0800

    chore: 解决部署不运行中

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "Starting SpringBoot Application"
 pwd
-export JENKINS_NODE_COOKIE=dontKillMe
 nohup java -Dhudson.util.ProcessTree.disable=true -jar target/*.jar --spring.profiles.active=prod &
