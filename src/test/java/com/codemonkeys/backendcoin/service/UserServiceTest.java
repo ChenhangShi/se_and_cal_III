a9bcf53930678753d5d3dceee43ea6cf02ca0f71
shichenhang <181250124@smail.nju.edu.cn>
2021-06-18 16:02:49 +0800

    fix: 测试推荐的用户id

--- src/test/java/com/codemonkeys/backendcoin/service/UserServiceTest.java
+++ src/test/java/com/codemonkeys/backendcoin/service/UserServiceTest.java
@@ -21,25 +21,25 @@ public class UserServiceTest {
 
     @Test
     public void testRecommend() throws Exception{
-        userService.addUserMovie(1,"长江7号");
-        userService.addUserMovie(1,"唐伯虎点秋香");
-        userService.addUserActor(1,"周星驰");
-        userService.addUserActor(1,"张家辉");
-        userService.addUserDirector(1,"周星驰");
-        userService.addUserDirector(1,"徐克");
-        userService.addUserGenre(1,"科幻");
-        userService.addUserGenre(1,"动画");
-        Set<String> res = userService.getUserRecommendedMovies(1);
+        userService.addUserMovie(Integer.MAX_VALUE,"长江7号");
+        userService.addUserMovie(Integer.MAX_VALUE,"唐伯虎点秋香");
+        userService.addUserActor(Integer.MAX_VALUE,"周星驰");
+        userService.addUserActor(Integer.MAX_VALUE,"张家辉");
+        userService.addUserDirector(Integer.MAX_VALUE,"周星驰");
+        userService.addUserDirector(Integer.MAX_VALUE,"徐克");
+        userService.addUserGenre(Integer.MAX_VALUE,"科幻");
+        userService.addUserGenre(Integer.MAX_VALUE,"动画");
+        Set<String> res = userService.getUserRecommendedMovies(Integer.MAX_VALUE);
         for(String movieName:res)
             System.out.println(movieName);
 
         System.out.println("\n-------------\n");
 
-        userService.deleteUserMovie(new UserMovieVO(){{setUserId(1);setMovie("唐伯虎点秋香");}});
-        userService.deleteUserActor(new UserActorVO(){{setUserId(1);setActor("周星驰");}});
-        userService.deleteUserDirector(new UserDirectorVO(){{setUserId(1);setDirector("徐克");}});
-        userService.deleteUserGenre(new UserGenreVO(){{setUserId(1);setGenre("动画");}});
-        res = userService.getUserRecommendedMovies(1);
+        userService.deleteUserMovie(new UserMovieVO(){{setUserId(Integer.MAX_VALUE);setMovie("唐伯虎点秋香");}});
+        userService.deleteUserActor(new UserActorVO(){{setUserId(Integer.MAX_VALUE);setActor("周星驰");}});
+        userService.deleteUserDirector(new UserDirectorVO(){{setUserId(Integer.MAX_VALUE);setDirector("徐克");}});
+        userService.deleteUserGenre(new UserGenreVO(){{setUserId(Integer.MAX_VALUE);setGenre("动画");}});
+        res = userService.getUserRecommendedMovies(Integer.MAX_VALUE);
         for(String movieName:res)
             System.out.println(movieName);
     }
